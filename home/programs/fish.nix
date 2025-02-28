0fab7e8d8eaf9133232e40e3c07786ee95cdb28e
Rouzihiro <ryossj@gmail.com>
2025-02-28 15:22:41 +0100

    update

--- home/programs/fish.nix
+++ home/programs/fish.nix
@@ -127,6 +127,19 @@ in
       }
     ];
     shellAliases = myAliases;
+    interactiveShellInit = ''
+    # Start keychain and add the SSH key
+    if command -v keychain > /dev/null
+        if not set -q SSH_AUTH_SOCK
+            echo "Initializing keychain..."
+            eval (keychain --eval --agents ssh ~/.ssh/HP-Nixo --quiet)
+            echo "Keychain initialization complete."
+        end
+    else
+        echo "keychain is not installed. Please install it to manage SSH keys."
+    end
+'';
+
     shellInit = ''
       # Set GPG TTY
       set -gx GPG_TTY (tty)
