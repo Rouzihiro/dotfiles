8d5cc81de302c86bd41f65179b5cb8b120ce5255
Rouzihiro <ryossj@gmail.com>
2025-03-23 23:33:24 +0100

    LF - new scripts

--- home/programs/fastfetch.nix
+++ home/programs/fastfetch.nix
@@ -1,133 +1,128 @@
 {
   programs.fastfetch = {
     enable = true;
+
     settings = {
+      display = {
+        color = {
+          keys = "35";
+          output = "95";
+        };
+        separator = " ➜  ";
+      };
 
-      "$schema" = "https://github.com/fastfetch-cli/fastfetch/raw/dev/doc/json_schema.json";
+      logo = {
+        source = ./triangle.png;
+        type = "sixel";
+        height = 20;
+        width = 40;
+        padding = {
+          top = 2;
+          left = 2;
+        };
+      };
 
-      "logo" = { "padding" = { "top" = 2; }; "type" = "auto"; };
-
-      "display" = { "separator" = " ➜  "; };
-
-      "modules" = [
-        {
-          "type" = "os";
-          "key" = " DISTRO";
-          "keyColor" = "31";
-        }
-        {
-          "type" = "kernel";
-          "key" = " ├  ";
-          "keyColor" = "31";
-        }
-        {
-          "type" = "packages";
-          "key" = " ├ 󰏖 ";
-          "keyColor" = "31";
-        }
-        {
-          "type" = "shell";
-          "key" = " └  ";
-          "keyColor" = "31";
-        }
-
+      modules = [
         "break"
-
         {
-          "type" = "wm";
-          "key" = " DE/WM";
-          "keyColor" = "32";
+          type = "os";
+          key = "OS - Rey-OS";
+          keyColor = "31";
         }
         {
-          "type" = "terminal";
-          "key" = " ├  ";
-          "keyColor" = "32";
+          type = "kernel";
+          key = " ├  ";
+          keyColor = "31";
         }
         {
-          "type" = "wmtheme";
-          "key" = " ├ 󰉼 ";
-          "keyColor" = "32";
+          type = "packages";
+          key = " ├ 󰏖 ";
+          keyColor = "31";
         }
         {
-          "type" = "icons";
-          "key" = " ├ 󰀻 ";
-          "keyColor" = "32";
+          type = "shell";
+          key = " └  ";
+          keyColor = "31";
         }
-        {
-          "type" = "cursor";
-          "key" = " ├  ";
-          "keyColor" = "32";
-        }
-        {
-          "type" = "terminalfont";
-          "key" = " └  ";
-          "keyColor" = "32";
-        }
-
         "break"
-
         {
-          "type" = "host";
-          "key" = "󰌢 SYSTEM";
-          "keyColor" = "33";
+          type = "wm";
+          key = "WM   ";
+          keyColor = "32";
         }
         {
-          "type" = "cpu";
-          "key" = " ├  ";
-          "keyColor" = "33";
+          type = "wmtheme";
+          key = " ├ 󰉼 ";
+          keyColor = "32";
         }
         {
-          "type" = "gpu";
-          "key" = " ├ 󰢮 ";
-          "keyColor" = "33";
+          type = "icons";
+          key = " ├ 󰀻 ";
+          keyColor = "32";
         }
         {
-          "type" = "memory";
-          "key" = " ├  ";
-          "keyColor" = "33";
+          type = "cursor";
+          key = " ├  ";
+          keyColor = "32";
         }
         {
-          "type" = "swap";
-          "key" = " ├  ";
-          "keyColor" = "33";
+          type = "terminal";
+          key = " ├  ";
+          keyColor = "32";
         }
         {
-          "type" = "disk";
-          "key" = " ├  ";
-          "keyColor" = "33";
+          type = "terminalfont";
+          key = " └  ";
+          keyColor = "32";
         }
-        {
-          "type" = "display";
-          "key" = " ├  ";
-          "keyColor" = "33";
-        }
-        {
-          "type" = "battery";
-          "key" = " └  ";
-          "keyColor" = "33";
-        }
-
         "break"
-
         {
-          "type" = "wifi";
-          "key" = "󰀂 NETWORK";
-          "keyColor" = "34";
+          type = "host";
+          format = "{5} {1} Type {2}";
+          key = "PC   ";
+          keyColor = "33";
         }
         {
-          "type" = "localip";
-          "key" = " ├ 󰒍 ";
-          "keyColor" = "34";
+          type = "cpu";
+          format = "{1} ({3}) @ {7} GHz";
+          key = " ├  ";
+          keyColor = "33";
         }
         {
-          "type" = "dns";
-          "key" = " ├ 󰇖 ";
-          "keyColor" = "34";
+          type = "gpu";
+          format = "{1} {2} @ {12} GHz";
+          key = " ├ 󰢮 ";
+          keyColor = "33";
         }
         {
-          "type" = "publicip";
-          "key" = " └ 󰩠 ";
-          "keyColor" = "34";
+          type = "memory";
+          key = " ├  ";
+          keyColor = "33";
+        }
+        {
+          type = "disk";
+          key = " ├ 󰋊 ";
+          keyColor = "33";
+        }
+        {
+          type = "monitor";
+          key = " ├  ";
+          keyColor = "33";
+        }
+        {
+          type = "player";
+          key = " ├ 󰥠 ";
+          keyColor = "33";
+        }
+        {
+          type = "media";
+          key = " └ 󰝚 ";
+          keyColor = "33";
+        }
+        "break"
+        {
+          type = "uptime";
+          key = "   Uptime   ";
         }
       ];
     };
