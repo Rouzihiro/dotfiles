1866b5580e1b66eb1666d70b6bcf5bce2f668be0
Rouzihiro <ryossj@gmail.com>
2025-04-08 11:01:30 +0200

    Qtile: opacity set in foot directly with mkForce. NICE !

--- home/programs/foot.nix
+++ home/programs/foot.nix
@@ -1,12 +1,19 @@
+{ lib, ... }:
 {
   programs.foot = {
     enable = true;
     server.enable = true;
     settings = {
-      main = { pad = "20x20"; }; 
+			  colors = {
+      		background = "000000";  # Black background
+      		foreground = "ffffff";  # White text
+      		alpha = lib.mkForce "0.8";
+				};
+			  main = {
+      		pad = "20x20";
+				};
       mouse = { hide-when-typing = "yes"; };
       tweak = { font-monospace-warn = "no"; };
-    
       };
   };
 }
