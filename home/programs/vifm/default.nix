c37f6b779a48e6e1b5f985e2106226281e66e2f4
Rouzihiro <ryossj@gmail.com>
2025-04-16 22:00:45 +0200

    +vfim: catppuccin mocha theme

--- home/programs/vifm/default.nix
+++ home/programs/vifm/default.nix
@@ -13,8 +13,10 @@
     enable = true;
   };
   home.file.".config/vifm/scripts/ffprobe.sh".source = ./ffprobe.sh;
-  home.file.".config/vifm/colors/hunter.vifm".source = ./hunter.vifm;
-  home.file.".config/vifm/colors/hunterv2.vifm".source = ./hunterv2.vifm;
+ 	home.file.".config/vifm/colors/mocha".source = ./mocha.vifm;
+	home.file.".config/vifm/colors/mochav2".source = ./mochav2.vifm;
+  #home.file.".config/vifm/colors/hunter.vifm".source = ./hunter.vifm;
+  #home.file.".config/vifm/colors/hunterv2.vifm".source = ./hunterv2.vifm;
   home.file.".config/vifm/vifmrc".source = ./vifmrc;
 
 	    home.packages = with pkgs; [
