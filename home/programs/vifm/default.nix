a0ca0bc2c463446d5c497320a94b0a8c788d28d0
Rouzihiro <ryossj@gmail.com>
2025-04-16 23:58:33 +0200

    vifm: catppuccin mocha custom-theme now working with opacity, NICE

--- home/programs/vifm/default.nix
+++ home/programs/vifm/default.nix
@@ -14,7 +14,6 @@
   };
   home.file.".config/vifm/scripts/ffprobe.sh".source = ./ffprobe.sh;
  	home.file.".config/vifm/colors/mocha.vifm".source = ./mocha.vifm;
-	home.file.".config/vifm/colors/mochav2.vifm".source = ./mochav2.vifm;
   home.file.".config/vifm/colors/hunter.vifm".source = ./hunter.vifm;
   home.file.".config/vifm/colors/hunterv2.vifm".source = ./hunterv2.vifm;
   home.file.".config/vifm/vifmrc".source = ./vifmrc;
