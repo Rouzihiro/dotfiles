fd43da618913b93259b174f6bde7e54f54e91057
Rouzihiro <ryossj@gmail.com>
2025-04-17 13:59:03 +0200

    +VIFM-Theme:Catppuccin Mocha (Custom, Starship inspired) ColorScheme

--- home/programs/vifm/default.nix
+++ home/programs/vifm/default.nix
@@ -9,20 +9,21 @@
   };
 
   config = lib.mkIf config.tools.vifm.enable {
-  programs.vifm = {
-    enable = true;
-  };
-  home.file.".config/vifm/scripts/ffprobe.sh".source = ./ffprobe.sh;
- 	home.file.".config/vifm/colors/mocha.vifm".source = ./mocha.vifm;
-  home.file.".config/vifm/colors/hunter.vifm".source = ./hunter.vifm;
-  home.file.".config/vifm/colors/hunterv2.vifm".source = ./hunterv2.vifm;
-  home.file.".config/vifm/vifmrc".source = ./vifmrc;
+    programs.vifm = {
+      enable = true;
+    };
+    home.file.".config/vifm/scripts/ffprobe.sh".source = ./ffprobe.sh;
+    home.file.".config/vifm/colors/mocha.vifm".source = ./mocha.vifm;
+    home.file.".config/vifm/colors/starship.vifm".source = ./starship.vifm;
+    home.file.".config/vifm/colors/hunter.vifm".source = ./hunter.vifm;
+    home.file.".config/vifm/colors/hunterv2.vifm".source = ./hunterv2.vifm;
+    home.file.".config/vifm/vifmrc".source = ./vifmrc;
 
-	    home.packages = with pkgs; [
+    home.packages = with pkgs; [
       poppler_utils
       sxiv
       ffmpegthumbnailer
       xan
     ];
-};
+  };
 }
