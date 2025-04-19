fba532635539d52019c7571a3592e01a814fc5e2
Rouzihiro <ryossj@gmail.com>
2025-04-19 18:26:51 +0200

    mpv-cfg mod

--- home/programs/mpv.nix
+++ home/programs/mpv.nix
@@ -11,13 +11,19 @@ in {
     programs.${name} = {
       enable = true;
       config = {
-        sub-ass-vsfilter-aspect-compat = "no";
+				sub-ass-use-video-data = "aspect-ratio";
         audio-device = "pulse/bluez_output.F8_4D_89_58_BC_65.1";
+				hwdec = "auto-safe";
+        vo = "gpu";
+        profile = "gpu-hq";
+
         # volume = 100;   # Uncomment if needed
         # hwdec = "auto"; # Uncomment for hardware decoding
       };
       bindings = {
         "Ctrl+f" = "cycle-values video-rotate 0 180";
+				"Alt+LEFT" = "add chapter -1";
+        "Alt+RIGHT" = "add chapter 1";
       };
     };
   };
