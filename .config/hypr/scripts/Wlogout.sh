38fb83fc1a466179b60cd5e1faa547fb84923295
Rouzihiro <ryossj@gmail.com>
2025-07-20 10:05:10 +0200

    updates for tmux. scripts

--- .config/hypr/scripts/Wlogout.sh
+++ .config/hypr/scripts/Wlogout.sh
@@ -1,20 +1,24 @@
 #!/bin/bash
 
-action=$(zenity --list --title="System Power Options" --column="Action" "Power Off" "Reboot" "Cancel")
+action=$(zenity --list \
+  --title="System Power Options" \
+  --text="Choose an action:" \
+  --width=300 --height=180 \
+  --column="Action" "Power Off" "Reboot" "Cancel")
 
 case "$action" in
   "Power Off")
-    if zenity --question --text="Are you sure you want to power off?"; then
+    if zenity --question --text="Are you sure you want to power off?" --width=250; then
       systemctl poweroff
     fi
     ;;
   "Reboot")
-    if zenity --question --text="Are you sure you want to reboot?"; then
+    if zenity --question --text="Are you sure you want to reboot?" --width=250; then
       systemctl reboot
     fi
     ;;
   "Cancel" | *)
-    # Do nothing or exit
+    # Do nothing
     ;;
 esac
 
