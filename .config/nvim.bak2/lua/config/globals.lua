84eb24e6af7f9a6b1043f5dc889edd4da2625f93
Rouzihiro <ryossj@gmail.com>
2025-07-30 01:33:24 +0200

    Big updates & cleanup. testing needed

---
+++ .config/nvim.bak2/lua/config/globals.lua
@@ -0,0 +1,89 @@
+-- Netrw settings
+vim.g.netrw_banner = 0
+vim.g.netrw_liststyle = 3
+vim.g.netrw_browse_split = 4
+vim.g.netrw_winsize = 25
+vim.g.netrw_altv = 1
+
+vim.g.macro_recording = ""
+
+-- Global variable to control borders for every plugins
+vim.g.border_style = "rounded"
+
+vim.g.border = {
+  { "╭", "FloatBorder" },
+  { "─", "FloatBorder" },
+  { "╮", "FloatBorder" },
+  { "│", "FloatBorder" },
+  { "╯", "FloatBorder" },
+  { "─", "FloatBorder" },
+  { "╰", "FloatBorder" },
+  { "│", "FloatBorder" },
+}
+
+vim.g.kind_icons = {
+  Supermaven = "",
+  Codeium = "",
+  Array = "󰅪",
+  Boolean = "⊨",
+  Key = "󰌆",
+  Namespace = "󰅪",
+  Null = "NULL",
+  Number = "#",
+  Object = "󰀚",
+  Package = "󰏗",
+  String = "󰀬",
+  TypeParameter = "󰊄",
+  Text = "󰉿",
+  Method = "󰆧",
+  Function = "󰊕",
+  Constructor = "",
+  Field = "󰜢",
+  Variable = "󰀫",
+  Class = "󰠱",
+  Interface = "",
+  Module = "",
+  Property = "󰜢",
+  Unit = "󰑭",
+  Value = "󰎠",
+  Enum = "",
+  Keyword = "󰌋",
+  Snippet = "",
+  Color = "󰏘",
+  File = "󰈙",
+  Reference = "󰈇",
+  Folder = "󰉋",
+  EnumMember = "",
+  Constant = "󰏿",
+  Struct = "󰙅",
+  Event = "",
+  Operator = "󰆕",
+}
+
+vim.g.loaded_gzip = 1
+vim.g.loaded_matchit = 1
+vim.g.loaded_matchparen = 1
+vim.g.loaded_netrwPlugin = 1
+vim.g.loaded_rplugin = 1
+vim.g.loaded_tarPlugin = 1
+vim.g.loaded_tutor = 1
+vim.g.loaded_zipPlugin = 1
+vim.g.loaded_2html_plugin = 1
+vim.g.loaded_osc52 = 1
+vim.g.loaded_tohtml = 1
+vim.g.loaded_getscript = 1
+vim.g.loaded_getscriptPlugin = 1
+vim.g.loaded_logipat = 1
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwSettings = 1
+vim.g.loaded_netrwFileHandlers = 1
+vim.g.loaded_tar = 1
+vim.g.loaded_rrhelper = 1
+vim.g.loaded_zip = 1
+vim.g.loaded_syntax = 1
+vim.g.loaded_synmenu = 1
+vim.g.loaded_optwin = 1
+vim.g.loaded_bugreport = 1
+vim.g.loaded_ftplugin = 1
+vim.g.loaded_vimball = 1
+vim.g.loaded_vimballPlugin = 1
