c1a6bf255f36ab8e8e76f4f7274656082086f5b4
Rouzihiro <ryossj@gmail.com>
2025-08-13 17:12:58 +0200

    fzf_selector script, new video-tool with fzf, nvim snippet for the script

--- .config/nvim/snippets/all.lua
+++ .config/nvim/snippets/all.lua
@@ -1,14 +1,16 @@
+
 ---@diagnostic disable: undefined-global
 
 return {
-	s("date", t(os.date("%Y/%m/%d"))),
-	s("time", t(os.date("%H:%M:%S"))),
-	s("mail", t("ryossj@gmail.com")),
-	s("email", t("ryossj@gmail.com")),
-	s("uvm", t("@uvm.edu")),
-	s("gh", t("github.com/Rouzihiro")),
-	s("(", { t("("), i(1), t(")") }),
-	s("[", { t("["), i(1), t("]") }),
-	s("{", { t("{"), i(1), t("}") }),
-	s("$", { t("$"), i(1), t("$") }),
+    s("date", t(os.date("%Y/%m/%d"))),
+    s("time", t(os.date("%H:%M:%S"))),
+    s("mail", t("ryossj@gmail.com")),
+    s("email", t("ryossj@gmail.com")),
+    s("uvm", t("@uvm.edu")),
+    s("gh", t("github.com/Rouzihiro")),
+    s("(", { t("("), i(1), t(")") }),
+    s("[", { t("["), i(1), t("]") }),
+    s("{", { t("{"), i(1), t("}") }),
+    s("$", { t("$"), i(1), t("$") }),
+    s("script", t({ "#!/bin/bash", "source fzf_selector.sh" }), i(1)),
 }
