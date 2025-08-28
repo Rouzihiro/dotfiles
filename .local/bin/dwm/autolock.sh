7799f0b2cd9b366ff2ad51a16eb46760f0f4e316
Rouzihiro <ryossj@gmail.com>
2025-08-28 10:52:33 +0200

    trying to fix the lockscreen crashes.

--- .local/bin/dwm/autolock.sh
+++ .local/bin/dwm/autolock.sh
@@ -1,12 +1,13 @@
-#!/usr/bin/bash
+#!/bin/sh
+# Autolock daemon script
 # Idle lock after 5 minutes
-(
-    while true; do
-        idle_time=$(xprintidle)  # milliseconds
-        if [ "$idle_time" -gt 300000 ]; then
-            i3lock -i ~/Pictures/lockscreen/lock_scaled.png
-        fi
-        sleep 30
-    done
-) &
 
+while true; do
+    idle_time=$(xprintidle)  # milliseconds
+    if [ "$idle_time" -gt 300000 ]; then
+        # This calls your simple lock command
+        i3lock --color 000000 --nofork
+				# i3lock --image ~/Pictures/lockscreen/lock_scaled.png --nofork
+    fi
+    sleep 30
+done
