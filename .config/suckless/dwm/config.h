ada63253fc488bea8a7190f44ab5df1654758cf2
Rouzihiro <ryossj@gmail.com>
2025-09-08 22:36:50 +0200

    everforest themes added

--- .config/suckless/dwm/config.h
+++ .config/suckless/dwm/config.h
@@ -39,22 +39,12 @@ void changeopacity(const Arg *arg);
 #define ICONSIZE 20   /* icon size */
 #define ICONSPACING 3 /* space between icon and title */
 
-static const char *fonts[]          = {  "JetBrainsMono Nerd Font:antialias=true:autohint=true:size=11", "Noto Sans CJK JP:size=11"  };
+/* Pick your theme */
+#include "themes/everforest-hard.h"
+// #include "themes/everforest-soft.h"
+// #include "themes/kanagawa.h"
 
-/* Color Scheme — Kanagawa colors */
-static const char normal_bar_background[]          = "#1f1f28"; // sumi ink #1
-static const char selected_bar_background[]        = "#2f2f3f"; // sumi ink #2
-static const char normal_bar_foreground[]          = "#dcd7ba"; // old paper
-static const char selected_bar_foreground[]        = "#c8c093"; // fuji gray
-static const char normal_window_border[]           = "#4f4f5f"; // sumi shadow
-static const char selected_window_border[]         = "#c8c093"; // fuji gray
-static const char special_normal_window_border[]   = "#7f7f87"; // sumi middle
-static const char special_selected_window_border[] = "#7fbbb3"; // wave green
-static const char *colors[][3]      = {
-	/*               fg         bg         border   */
-   [SchemeNorm] = { normal_bar_foreground, normal_bar_background, normal_window_border },
-   [SchemeSel]  = { selected_bar_foreground, selected_bar_background, selected_window_border  },
-};
+static const char *fonts[]          = {  "JetBrainsMono Nerd Font:antialias=true:autohint=true:size=11", "Noto Sans CJK JP:size=11"  };
 
 
 /* tagging */
