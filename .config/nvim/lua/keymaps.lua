780936f21b2f5d787c92c1594943940df16c5d68
Rouzihiro <ryossj@gmail.com>
2025-09-18 14:28:49 +0200

    new keymaps

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -1,14 +1,30 @@
 local map = vim.keymap.set
-
 vim.g.mapleader = " "
+
+-- window management
+-- map("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" })
+-- map("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" })
+map("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" })
+map("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" })
+map('n', '<leader>B', '<Cmd>e #<CR>') -- Edit the alternate file (#)
+map('n', '<leader>sh', '<Cmd>bot sf #<CR>', { desc = "Horizontal split with alternate file" })
+map('n', '<leader>sv', '<Cmd>vert belowright sf #<CR>', { desc = "Vertical split with alternate file" })
+
+-- tabs
+map("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" })
+map("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" })
+map("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" })
+map("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" })
+map("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" })
+map("n", "<leader>ta", "<cmd>tab all<CR>", { desc = "Open all current NVIM files in their own tab" })
 
 map("n", "<leader>e", function()
 	vim.cmd("Oil --float " .. vim.loop.cwd())
 end, { desc = "Open Oil floating file manager" })
 
 map('n', '<leader>u', '<Cmd>update<CR> <Cmd>source<CR>')
+map('n', '<leader>w', '<Cmd>write<CR>')
 map('n', '<leader>s', '<Cmd>write<CR>')
--- map({ 'n', 'v', 'x' }, '<leader>s', '<Cmd>e #<CR>')
 map('n', '<leader>q', '<Cmd>bd<CR>', { desc = "Close buffer" })
 map("n", "K", vim.lsp.buf.hover, { desc = "LSP Hover" })
 
@@ -21,15 +37,12 @@ map('n', '<leader>h', "<Cmd>Pick help<CR>")
 map('n', '<leader>b', "<Cmd>Pick buffers<CR>")
 map('n', '<leader>lf', vim.lsp.buf.format)
 map('i', '<c-Space>', function() vim.lsp.completion.get() end)
-map('n', '<leader>t', '<Cmd>Open .<CR>')
+map('n', '<leader>tt', '<Cmd>Open .<CR>')
 map('n', '<leader>nc', '<Cmd>e $MYVIMRC<CR>')
 map('n', '<leader>nn', '<Cmd>e ~/.config/nvim/lua/keymaps.lua<CR>')
 map('n', '<leader>zz', '<Cmd>e ~/.config/zsh/.aliases<CR>')
 map('n', '<leader>zc', '<Cmd>e ~/.config/zsh/.zshrc<CR>')
 map('n', '<leader>zf', '<Cmd>e ~/.config/zsh/.aliases-functions<CR>')
-map('n', '<leader>B', '<Cmd>e #<CR>') -- Edit the alternate file (#)
-map('n', '<leader>hs', '<Cmd>bot sf #<CR>', { desc = "Horizontal split with alternate file" })
-map('n', '<leader>vs', '<Cmd>vert belowright sf #<CR>', { desc = "Vertical split with alternate file" })
 map("n", "}", "}zz")
 map("n", "{", "{zz")
 
@@ -48,47 +61,36 @@ end
 map({ "n" }, "<leader>a", "mzA<space><esc>p`z", { desc = "paste to the end of line" })
 map("n", "<leader>za", 'ggVG"+y', { desc = "Yank entire buffer" })
 
--- Search/replace
-map({ "n", "v" }, "<leader>C", "<Cmd>%s/<C-r><C-w>//g<Left><Left>", { desc = "Replace word under cursor" })
+map({ "n", "v" }, "<leader>rw",
+	":%s/<C-r><C-w>//g<Left><Left>",
+	{ desc = "Replace word under cursor" })
 
-map({ "n", "v" }, "<leader>c", "<Cmd>%s/<C-r><C-w>//gc<Left><Left><Left>",
+map({ "n", "v" }, "<leader>rW",
+	":%s/<C-r><C-w>//gc<Left><Left><Left>",
 	{ desc = "Replace word under cursor with confirmation" })
 
-
--- map({ 'n', 'v' }, '<leader>c', '1z=', { desc = "Correct last misspelled word" })
+map({ 'n', 'v' }, '<leader>c', '1z=', { desc = "Correct last misspelled word" })
 
 -- Formatting
 map("n", "<leader>al", "<cmd>lua vim.lsp.buf.format()<CR>", { desc = "Format with LSP" })
-
-
--- Spell control
--- map("n", "<leader>gg", "<cmd>lua vim.diagnostic.open_float()<CR>", { desc = "Show grammar issues" })
--- map("n", "<leader>ss", "<cmd>set spell!<CR>", { desc = "Toggle spell check" })
--- map("n", "<leader>se", "<cmd>set spelllang=en_us<CR>", { desc = "English spell" })
--- map("n", "<leader>sd", "<cmd>set spelllang=de<CR>", { desc = "German spell" })
--- map("n", "z=", "z=1<CR>", { silent = true, desc = "Accept 1st suggestion" })
-
 
 map('n', '<leader>>', '<Cmd>vertical resize +5<CR>', { desc = 'Increase split width' })
 map('n', '<leader><', '<Cmd>vertical resize -5<CR>', { desc = 'Decrease split width' })
 map('n', '<leader>+', '<Cmd>resize +5<CR>', { desc = 'Increase split height' })
 map('n', '<leader>-', '<Cmd>resize -5<CR>', { desc = 'Decrease split height' })
 
--- map('n', '<leader>vs', '<Cmd>vsplit<CR>', { desc = 'Vertical split' })
--- map('n', '<leader>hs', '<Cmd>split<CR>', { desc = 'Horizontal split' })
-
 -- Telescope
 local builtin = require("telescope.builtin")
 map("n", "<leader>o", builtin.oldfiles, { desc = "Recent files" })
-map("n", "<leader>f", builtin.find_files, { desc = "Find files" })
+map("n", "<leader>ff", builtin.find_files, { desc = "Find files" })
 -- map("n", "<leader>B", builtin.buffers, { desc = "Buffers" })
-map("n", "<leader>H", builtin.help_tags, { desc = "Help tags" })
+map("n", "<leader>fh", builtin.help_tags, { desc = "Help tags" })
 
-map('n', '<leader>g', builtin.live_grep, { desc = '[F]ind by [G]rep (search content)' })
-map('n', '<leader>w', function()
+map('n', '<leader>fg', builtin.live_grep, { desc = '[F]ind by [G]rep (search content)' })
+map('n', '<leader>fw', function()
 	builtin.grep_string({ search = vim.fn.expand('<cword>') })
 end, { desc = '[F]ind current [W]ord' })
-map('n', '<leader>W', function()
+map('n', '<leader>fd', function()
 	builtin.live_grep({ search_dirs = { vim.fn.input('Directory: ') } })
 end, { desc = '[F]ind in [D]irectory' })
 
@@ -130,3 +132,10 @@ vim.api.nvim_create_autocmd("BufWritePost", {
 		end
 	end,
 })
+
+-- Spell control
+-- map("n", "<leader>gg", "<cmd>lua vim.diagnostic.open_float()<CR>", { desc = "Show grammar issues" })
+-- map("n", "<leader>ss", "<cmd>set spell!<CR>", { desc = "Toggle spell check" })
+-- map("n", "<leader>se", "<cmd>set spelllang=en_us<CR>", { desc = "English spell" })
+-- map("n", "<leader>sd", "<cmd>set spelllang=de<CR>", { desc = "German spell" })
+-- map("n", "z=", "z=1<CR>", { silent = true, desc = "Accept 1st suggestion" })
