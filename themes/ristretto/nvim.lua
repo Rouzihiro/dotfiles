78b98d17f1b1e08612ace16c2e07b73cc29ce06d
Rouzihiro <ryossj@gmail.com>
2025-11-05 23:42:33 +0100

    update

---
+++ themes/ristretto/nvim.lua
@@ -0,0 +1,27 @@
+local M = {}
+
+M.default_color = "monokai-pro"
+
+M.setup = function()
+	require("monokai-pro").setup({
+		filter = "ristretto",
+		override = function()
+			return {
+				NonText = { fg = "#948a8b" },
+				MiniIconsGrey = { fg = "#948a8b" },
+				MiniIconsRed = { fg = "#fd6883" },
+				MiniIconsBlue = { fg = "#85dacc" },
+				MiniIconsGreen = { fg = "#adda78" },
+				MiniIconsYellow = { fg = "#f9cc6c" },
+				MiniIconsOrange = { fg = "#f38d70" },
+				MiniIconsPurple = { fg = "#a8a9eb" },
+				MiniIconsAzure = { fg = "#a8a9eb" },
+				MiniIconsCyan = { fg = "#85dacc" }, -- same value as MiniIconsBlue for consistency
+			}
+		end,
+	})
+
+	vim.cmd.colorscheme(M.default_color)
+end
+
+return M
