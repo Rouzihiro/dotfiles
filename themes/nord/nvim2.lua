ee305dc9435ff174491c29e38568298ba3c1eb26
Rouzihiro <ryossj@gmail.com>
2025-11-05 23:42:33 +0100

    update

---
+++ themes/nord/nvim2.lua
@@ -0,0 +1,5 @@
+local M = {}
+
+M.default_color = "nord"
+
+return M
---
+++ themes/osaka-jade/nvim.lua
@@ -0,0 +1,11 @@
+local M = {}
+
+M.default_color = "bamboo"
+
+require("bamboo").setup({
+  style = "multiplex",
+  toggle_style_key = nil,
+})
+
+
+return M
