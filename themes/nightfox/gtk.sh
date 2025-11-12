905237a7e6d27f3776056c4c835db873afd0d3b5
Rouzihiro <ryossj@gmail.com>
2025-11-12 09:04:26 +0100

    updates

--- themes/nightfox/gtk.sh
+++ themes/nightfox/gtk.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 sleep 2  # Wait for D-Bus to be ready
 
-gsettings set org.gnome.desktop.interface gtk-theme 'Nightfox'
+gsettings set org.gnome.desktop.interface gtk-theme 'Nightfox-Dark'
 gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
 gsettings set org.gnome.desktop.interface icon-theme 'Yaru-blue'
 gsettings set org.gnome.desktop.interface cursor-theme 'Adwaita'
 gsettings set org.gnome.desktop.interface font-name 'DejaVu Sans Mono 10'
 
-export GTK_THEME=Nightfox
+export GTK_THEME=Nightfox-Dark
 export XCURSOR_THEME=Adwaita
