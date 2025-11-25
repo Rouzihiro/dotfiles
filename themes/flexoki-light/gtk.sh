f3dfb3eff77f3d48f4788fcda666e9fd75bcc613
Rouzihiro <ryossj@gmail.com>
2025-11-25 21:43:20 +0100

    cleanup. Flexoki Dark added !

--- themes/flexoki-light/gtk.sh
+++ themes/flexoki-light/gtk.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 sleep 2  # Wait for D-Bus to be ready
 
-gsettings set org.gnome.desktop.interface gtk-theme 'Flexoki-Light'
+gsettings set org.gnome.desktop.interface gtk-theme 'flexoki'
+gsettings set org.gnome.desktop.interface icon-theme 'Tela-brown-light'
+gsettings set org.gnome.desktop.interface cursor-theme 'la-capitaine-cursor-theme'
 gsettings set org.gnome.desktop.interface color-scheme 'prefer-light'
-gsettings set org.gnome.desktop.interface icon-theme 'Yaru-blue'
-gsettings set org.gnome.desktop.interface cursor-theme 'Adwaita'
-gsettings set org.gnome.desktop.interface font-name 'DejaVu Sans Mono 10'
+gsettings set org.gnome.desktop.interface cursor-size 24
 
-export GTK_THEME=Flexoki-Light
-export XCURSOR_THEME=Adwaita
+export XCURSOR_THEME=la-capitaine-cursor-theme
+# export GTK_THEME=flexoki
