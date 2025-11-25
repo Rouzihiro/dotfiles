41c3fbf0a311589e4d2f723aade53bda5b15a439
Rouzihiro <ryossj@gmail.com>
2025-11-25 15:06:30 +0100

    cleanup

---
+++ themes/nord/gtk.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+sleep 2  # Wait for D-Bus to be ready
+
+gsettings set org.gnome.desktop.interface icon-theme 'Tela-nord'
+gsettings set org.gnome.desktop.interface cursor-theme 'la-capitaine-cursor-theme'
+gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
+gsettings set org.gnome.desktop.interface cursor-size 24
+
+export XCURSOR_THEME=la-capitaine-cursor-theme
