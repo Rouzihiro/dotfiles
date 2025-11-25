f05e010de24612acc10597e9a69591dd74c361f4
Rouzihiro <ryossj@gmail.com>
2025-11-26 00:02:11 +0100

    best setup so far for handeling gtk themes

--- themes/ristretto/gtk.sh
+++ themes/ristretto/gtk.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 sleep 2  # Wait for D-Bus to be ready
 
-gsettings set org.gnome.desktop.interface gtk-theme 'Ristretto'
+gsettings set org.gnome.desktop.interface gtk-theme 'Material-Black'
 gsettings set org.gnome.desktop.interface icon-theme 'Tela-dark'
 gsettings set org.gnome.desktop.interface cursor-theme 'la-capitaine-cursor-theme'
 gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
 gsettings set org.gnome.desktop.interface cursor-size 24
 
 export XCURSOR_THEME=la-capitaine-cursor-theme
-# export GTK_THEME=Ristretto
+# export GTK_THEME=Material-Black
