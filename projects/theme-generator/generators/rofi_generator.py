a9a8cb222c3dbb5758d7aaa87c4ecbd7c6f9aa30
Rouzihiro <ryossj@gmail.com>
2025-11-28 15:02:00 +0100

    new theme generator, need to adjust rofi too now

--- projects/theme-generator/generators/rofi_generator.py
+++ projects/theme-generator/generators/rofi_generator.py
@@ -10,51 +10,35 @@ class RofiGenerator(BaseGenerator):
         self.backup_file(output_file)
         
         template = f"""* {{
-    primary: {self.colors['color3']};
-    primary-fixed: {self.colors['color3']};
-    primary-fixed-dim: {self.colors['color11']};
-    on-primary: {self.colors['color0']};
-    on-primary-fixed: {self.colors['color0']};
-    on-primary-fixed-variant: {self.colors['color8']};
-    primary-container: {self.colors['color4']};
-    on-primary-container: {self.colors['foreground']};
-    secondary: {self.colors['color7']};
-    secondary-fixed: {self.colors['color7']};
-    secondary-fixed-dim: {self.colors['color8']};
-    on-secondary: {self.colors['color0']};
-    on-secondary-fixed: {self.colors['color0']};
-    on-secondary-fixed-variant: {self.colors['color8']};
-    secondary-container: {self.colors['color6']};
-    on-secondary-container: {self.colors['foreground']};
-    tertiary: {self.colors['color15']};
-    tertiary-fixed: {self.colors['color15']};
-    tertiary-fixed-dim: {self.colors['color7']};
-    on-tertiary: {self.colors['color0']};
-    on-tertiary-fixed: {self.colors['color0']};
-    on-tertiary-fixed-variant: {self.colors['color8']};
-    tertiary-container: {self.colors['color5']};
-    on-tertiary-container: {self.colors['foreground']};
-    error: {self.colors['color1']};
-    on-error: {self.colors['color0']};
-    error-container: {self.colors['color9']};
-    on-error-container: {self.colors['foreground']};
-    surface: {self.colors['color0']};
-    on-surface: {self.colors['foreground']};
-    on-surface-variant: {self.colors['color7']};
-    outline: {self.colors['color8']};
-    outline-variant: {self.colors['color8']};
-    shadow: #000000;
-    scrim: #000000;
-    inverse-surface: {self.colors['foreground']};
-    inverse-on-surface: {self.colors['color0']};
-    inverse-primary: {self.colors['color4']};
-    surface-dim: {self.colors['color0']};
-    surface-bright: {self.colors['color8']};
-    surface-container-lowest: {self.colors['color0']};
-    surface-container-low: {self.colors['color0']};
-    surface-container: {self.colors['color8']};
-    surface-container-high: {self.colors['color8']};
-    surface-container-highest: {self.colors['color8']};
+    background: {self.colors['background']};
+    foreground: {self.colors['foreground']};
+    selection-background: {self.colors['selection_background']};
+    selection-foreground: {self.colors['selection_foreground']};
+    border-color: {self.colors['selection_background']};
+    
+    /* Normal colors */
+    color0: {self.colors['color0']};
+    color1: {self.colors['color1']};
+    color2: {self.colors['color2']};
+    color3: {self.colors['color3']};
+    color4: {self.colors['color4']};
+    color5: {self.colors['color5']};
+    color6: {self.colors['color6']};
+    color7: {self.colors['color7']};
+    
+    /* Bright colors */
+    color8: {self.colors['color8']};
+    color9: {self.colors['color9']};
+    color10: {self.colors['color10']};
+    color11: {self.colors['color11']};
+    color12: {self.colors['color12']};
+    color13: {self.colors['color13']};
+    color14: {self.colors['color14']};
+    color15: {self.colors['color15']};
+    
+    /* Extended colors */
+    color16: {self.colors['color16']};
+    color17: {self.colors['color17']};
 }}
 """
         
