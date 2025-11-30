2e56c2038162f2495d91c139ace69b6791d53bcd
Rouzihiro <ryossj@gmail.com>
2025-11-30 02:06:01 +0100

    sakura theme works, but needs adjustments

--- themes/sakura/nvim.lua
+++ themes/sakura/nvim.lua
@@ -1,11 +1,5 @@
-return {
-	{
-		"bjarneo/pixel.nvim",
-		name = "pixel",
-		lazy = false,
-		priority = 1000,
-		config = function()
-			vim.cmd.colorscheme("pixel")
-		end,
-	},
-}
+local M = {}
+
+M.default_color = "pixel"
+
+return M
