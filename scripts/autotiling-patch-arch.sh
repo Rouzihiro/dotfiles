6469bb4cf642ad5aa3e4dc8f90ed5e590405b745
Rouzihiro <ryossj@gmail.com>
2025-12-05 18:57:11 +0100

    OS patches included

--- scripts/autotiling-patch-arch.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-CONFIG_FILE="$HOME/.config/sway/config.d/autostart"
-
-[ -f "$CONFIG_FILE" ] || { echo "File not found!"; exit 1; }
-
-sed -i 's/autotiling-rs/autotiling/g' "$CONFIG_FILE" && \
-echo "✓ Updated $CONFIG_FILE (autotiling-rs → autotiling)"
