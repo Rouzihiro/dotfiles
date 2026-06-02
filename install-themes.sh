174ddd1840c06f57475d2cfa673990c6ef8dfa40
Rouzihiro <ryossj@gmail.com>
2026-06-02 19:17:11 +0200

    up

--- install-themes.sh
+++ install-themes.sh
@@ -49,17 +49,3 @@ ln -snf ~/.config/zorro/current/theme/mako.ini ~/.config/mako/config
 
 z-theme-cache
 python ~/dotfiles/projects/theme-generator/starship_generator.py
-
-# ya pkg add yazi-rs/flavors:catppuccin-mocha
-# ya pkg add yazi-rs/flavors:catppuccin-latte
-# ya pkg add BennyOe/tokyo-night
-# ya pkg add marcosvnmelo/kanagawa-dragon
-# ya pkg add dangooddd/kanagawa
-# ya pkg add matt-dong-123/gruvbox-material
-# ya pkg add Chromium-3-Oxide/everforest-medium
-# ya pkg add gosxrgxx/flexoki-dark
-# ya pkg add gosxrgxx/flexoki-light
-# ya pkg add Mintass/rose-pine-moon
-# ya pkg add Mintass/rose-pine
-# ya pkg add AdithyanA2005/nord
-
